cbba1a977ee4d4e97cff0c7405642302db27bdd2
kakitaku00 <webtrakakinuma@gmail.com>
2019-01-22 19:08:52 +0900

    FizzBuzz sample

---
+++ 2/2_1/2_1_2.rb
@@ -0,0 +1,13 @@
+# FizzBuzz問題とは
+# 3で割り切れる数値を引数に渡すと"Fizz"を返す
+# 5で割り切れる数値を引数に渡すと"Buzz"を返す
+# 15で
+
+# FizzBuzzプログラムの実行例
+# fizz_buzz(1) #=> "1"
+# fizz_buzz(2) #=> "2"
+# fizz_buzz(3) #=> "Fizz"
+# fizz_buzz(4) #=> "4"
+# fizz_buzz(5) #=> "Buzz"
+# fizz_buzz(6) #=> "Fizz"
+# fizz_buzz(15) #=> "Fizz Buzz"
