8a7edba0c7f54612137103178dac99e24e9f2fd4
kakitaku00 <webtrakakinuma@gmail.com>
2019-03-01 10:08:11 +0900

    コードをリファクタリングする

--- 4/4_6/lib/rgb.rb
+++ 4/4_6/lib/rgb.rb
@@ -1,8 +1,24 @@
 # to_hexメソッドを作成する
 
+# def to_hex(r, g, b)
+#   '#' +
+#     r.to_s(16).rjust(2, '0') +
+#     r.to_s(16).rjust(2, '0') +
+#     r.to_s(16).rjust(2, '0')
+# end
+
+# コードをリファクタリングする
 def to_hex(r, g, b)
-  '#' +
-    r.to_s(16).rjust(2, '0') +
-    r.to_s(16).rjust(2, '0') +
-    r.to_s(16).rjust(2, '0')
+  hex = '#'
+  [r, g, b].each do |n|
+    hex += n.to_s(16).rjust(2, '0')
+  end
+  hex
+end
+
+# injectメソッドを使ってもっと短く書く
+def to_hex(r, g, b)
+  [r, g, b].inject('#') do |hex, n|
+    hex + n.to_s(16).rjust(2, '0')
+  end
 end
